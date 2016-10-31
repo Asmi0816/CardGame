f500ab231a6e5c7670d5e32b5b91cdec3283814b
Arick Smith <kidwriter2020@gmail.com>
2016-10-31 12:29:32 -0600

    Made two new files and set them up to have code placed inside them.

---
+++ CardGame/Model/Deck.swift
@@ -0,0 +1,15 @@
+//
+//  Deck.swift
+//  CardGame
+//
+//  Created by Smith, Arick on 10/31/16.
+//  Copyright © 2016 Smith, Arick. All rights reserved.
+//
+
+import UIKit
+
+
+class Deck
+{
+    
+}
---
+++ CardGame/Model/PlayingCardDeck.swift
@@ -0,0 +1,15 @@
+//
+//  PlayingCardDeck.swift
+//  CardGame
+//
+//  Created by Smith, Arick on 10/31/16.
+//  Copyright © 2016 Smith, Arick. All rights reserved.
+//
+
+import UIKit
+
+
+class PlayingCardDeck : Deck
+{
+    
+}
