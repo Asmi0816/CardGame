2a9ae0f8160d7c83db2a016e07d598cb43d63584
Arick Smith <kidwriter2020@gmail.com>
2016-11-21 13:21:11 -0700

    Made a compellable bit of code for the view.

---
+++ CardGame/View/CardView.swift
@@ -0,0 +1,44 @@
+//
+//  CardView.swift
+//  CardGame
+//
+//  Created by Smith, Arick on 11/21/16.
+//  Copyright © 2016 Smith, Arick. All rights reserved.
+//
+
+import UIKit
+
+
+class CardView
+{
+    
+    internal var frontImage : UIImageView
+    internal var backImage : UIImageView
+    internal var isFaceUp : Bool
+    internal var bottomLabel : UILabel
+    internal var topLabel : UILabel
+    
+    init ()
+    {
+        frontImage = UIImageView()
+        backImage = UIImageView()
+        isFaceUp = Bool()
+        bottomLabel = UILabel()
+        topLabel = UILabel()
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
